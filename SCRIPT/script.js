eef343c6188c0950f476d21eff78ed1ce5b3dbf2
JeanBrgi <broggijean99@gmail.com>
2024-12-03 22:17:05 +0100

    Feature Bouton

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -2,6 +2,10 @@ var video = document.getElementById("bckvideo");
 var bouton = document.getElementById("btn");
 
 
+bouton.addEventListener("click", function () {
+    bouton.classList.toggle("onclic");
+});
+
 function pause() {
     if (video.paused) {
         video.play();
